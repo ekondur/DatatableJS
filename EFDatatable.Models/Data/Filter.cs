9e68aea03edbe977c5271972d1abfa9583cfacbe
Emrah KONDUR <emrahkondur@hotmail.com>
2019-05-12 23:19:48 +0300

    DataRequest and DataResult models added

---
+++ EFDatatable.Models/Data/Filter.cs
@@ -0,0 +1,9 @@
+﻿namespace EFDatatable.Models.Data
+{
+    public class Filter
+    {
+        public string Field { get; set; }
+        public string Value { get; set; }
+        public string Operand { get; set; }
+    }
+}
---
+++ EFDatatable.Models/Data/Order.cs
@@ -0,0 +1,8 @@
+﻿namespace EFDatatable.Models.Data
+{
+    public class Order
+    {
+        public int column { get; set; }
+        public string dir { get; set; }
+    }
+}
