d1913af4f2c072a71bf715fc9de4dbc982822ca0
ekondur <emrahkondur@hotmail.com>
2020-01-10 14:00:54 +0300

    moved test site

---
+++ EFDatatable.Web/Sql/EFContext.cs
@@ -0,0 +1,16 @@
+﻿using EFDatatable.Web.Models;
+using System.Data.Entity;
+
+namespace EFDatatable.Web.Sql
+{
+    public class EFContext : DbContext
+    {
+        public EFContext()
+            : base("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EFDb;Integrated Security=true;MultipleActiveResultSets=true")
+        {
+
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
