67be5d37a947633f882a299ba24149c522864baf
ekondur <emrahkondur@hotmail.com>
2020-02-06 09:07:03 +0300

    Commands added

---
+++ EFDatatable/Models/Command.cs
@@ -0,0 +1,16 @@
+﻿namespace EFDatatable
+{
+    /// <summary>
+    /// Define a command.
+    /// </summary>
+    public class Command
+    {
+        public string Title { get; private set; }
+        public string OnClick { get; private set; }
+        public Command(string title, string onClick)
+        {
+            Title = title;
+            OnClick = onClick;
+        }
+    }
+}
